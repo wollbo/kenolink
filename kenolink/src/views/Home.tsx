909c9c2eeb61096b1eaa99df8fc940fdbe6d9425
wollbo <hildingw@gmail.com>
2022-11-17 17:21:35 +0100

    typescript fel

--- kenolink/src/views/Home.tsx
+++ kenolink/src/views/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { VStack, Heading } from '@chakra-ui/react';
 
+
+
 const Home = () => {
+
   return (
     <VStack alignItems={'start'}>
       <Heading mb={8}>Home</Heading>
